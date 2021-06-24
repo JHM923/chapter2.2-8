c484c4cafe4cf3bef299ec4e43e63d21ec5afd65
username <1151555861@qq.com>
2021-06-24 13:51:05 +0800

    first commit

---
+++ chapter2.2-8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace chapter2._2_8
+{
+    class GenericCounter<T>
+    {
+        private static int value; //每个封闭的、已构造类型对应一个字段
+           static GenericCounter()
+          {
+            Console.WriteLine("Initializing counter for {0}", typeof(T));
+          }
+        public static void Increment()
+            {
+            value++;
+            }
+        public static void Display()
+        {
+            Console.WriteLine("Counter for {0}: {1}", typeof(T), value);
+        }
+    }
+    class GenericCounterDemo
+    {
+        static void Main()
+        {
+            GenericCounter<string>.Increment();
+            GenericCounter<string>.Increment();
+            GenericCounter<string>.Display();
+            GenericCounter<int>.Display();
+            GenericCounter<int>.Increment();
+            GenericCounter<int>.Display();
+            Console.ReadKey();
+        }
+    }
+}
